1f6f73f50fc7979ee196cf86177da782f4f9a297
Shinvey Hu <shinvey@qq.com>
2019-01-06 00:40:54 +0800

    test for integration with gitlab

--- src/todo/footer.jsx
+++ src/todo/footer.jsx
@@ -20,6 +20,7 @@ export default {
     return (
       <div id="footer" style={footerStyle}>
         <p>Hey Shinvey, Jenkins runs as your expected, you did it!</p>
+        <p>Hey Shinvey, Jenkins runs as your expected, you did it!</p>
         <p>Written by {this.author}</p>
       </div>
     )
